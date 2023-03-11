6256c486ebf3b38666da3ff3323ee19188f7a83b
Nikita Eskin <ladeuxmaster@gmail.com>
2023-03-11 19:26:52 +0300

    Replace lf with crlf

--- server/handlers/gameHandlers.js
+++ server/handlers/gameHandlers.js
@@ -4,7 +4,6 @@ const metadata = require('../store/metadata')*/
 
 module.exports = (io, socket) => {
     const moveClientHandler = data => {
-        // eslint-disable-next-line no-console
         store.rooms[data.roomId].engine.moveClientEntity(data)
     }
     socket.on('game:move-client', moveClientHandler)
